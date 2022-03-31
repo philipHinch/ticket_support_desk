8361fe6a10803b7ebc310f0cfa95f33959c0f957
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-03-31 16:36:08 +0300

    express setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...');
+// set up express and listen on specific port
+const express = require('express');
+// dotenv loads environment variables from a .env file into process.env
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${ PORT }`);
+});
